78e8cc000f830dc2d94a4bc0b9dafa13f85af5db
Cyril Müller <muellcy1@students.zhaw.ch>
2016-03-16 17:40:53 +0100

    readd tests

---
+++ sources/AvocadoShare/src/test/ch/avocado/share/EmailAddressVerificationTest.java
@@ -0,0 +1,24 @@
+package ch.avocado.share;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import ch.avocado.share.model.data.EmailAddressVerification;
+
+public class EmailAddressVerificationTest {
+
+	@Test
+	public void test_code_generation() {
+		String code = EmailAddressVerification.generateCode();
+		assertNotNull(code);
+		assertTrue(code.length() == 32);
+		for(char codeCharacter : code.toCharArray()) {
+			assertTrue(Character.isAlphabetic(codeCharacter) || Character.isDigit(codeCharacter));
+		}
+		System.out.println(code);
+	}
+}
---
+++ sources/AvocadoShare/src/test/ch/avocado/share/UserTest.java
@@ -0,0 +1,11 @@
+package ch.avocado.share;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+
+}
