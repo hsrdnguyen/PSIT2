0dbb0eb10cd52f46b9b4f825f87172e271472526
Sascha Bergmann <Sascha Bergmann>
2016-03-24 15:00:46 +0100

    added tests for user registration bean

--- sources/AvocadoShare/src/main/ch/avocado/share/controller/UserRegistrationBean.java
+++ sources/AvocadoShare/src/main/ch/avocado/share/controller/UserRegistrationBean.java
@@ -6,16 +6,23 @@ import ch.avocado.share.model.data.EmailAddressVerification;
 import ch.avocado.share.model.data.User;
 import ch.avocado.share.model.data.UserPassword;
 import ch.avocado.share.model.exceptions.ServiceNotFoundException;
+import ch.avocado.share.service.IMailingService;
 import ch.avocado.share.service.IUserDataHandler;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by bergm on 24/03/2016.
  */
-public class UserRegistrationBean {
+public class UserRegistrationBean implements Serializable {
+    private String name;
+    private String prename;
+    private String avatar;
+    private String emailAddress;
+    private String password;
 
-    public void addUser(String name, String prename, String avatar, String emailAddress, String password)
+    public void addUser()
     {
         if(name == null) throw new IllegalArgumentException("name is null");
         if(prename == null) throw new IllegalArgumentException("prename is null");
@@ -32,9 +39,57 @@ public class UserRegistrationBean {
 
         try {
             user.setId(ServiceLocator.getService(IUserDataHandler.class).addUser(user));
+            ServiceLocator.getService(IMailingService.class).sendVerificationEmail(user);
         } catch (ServiceNotFoundException e) {
             e.printStackTrace();
         }
+    }
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) throw new IllegalArgumentException("name is null");
+        this.name = name;
+    }
+
+    public String getPrename() {
+        return prename;
+    }
+
+    public void setPrename(String prename) {
+        if (prename == null) throw new IllegalArgumentException("prename is null");
+        this.prename = prename;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        if (avatar == null) throw new IllegalArgumentException("avatar is null");
+        this.avatar = avatar;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        if (emailAddress == null ) throw new IllegalArgumentException("emailAddress is null");
+        if (!emailAddress.toLowerCase().matches("[a-z0-9]+[_a-z0-9\\.-]*[a-z0-9]+@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})")) throw new IllegalArgumentException("emailAddress is not valid");
+
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password == null) throw new IllegalArgumentException("password is null");
+        this.password = password;
     }
 }
