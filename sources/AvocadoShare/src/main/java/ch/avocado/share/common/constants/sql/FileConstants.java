0919e19e6bd767e5b83a36374e9ec385591addb8
muellcy1 <muellcy1@students.zhaw.ch>
2016-05-02 17:13:37 +0200

    Query whole file information when searching

--- sources/AvocadoShare/src/main/java/ch/avocado/share/common/constants/sql/FileConstants.java
+++ sources/AvocadoShare/src/main/java/ch/avocado/share/common/constants/sql/FileConstants.java
@@ -4,10 +4,18 @@ package ch.avocado.share.common.constants.sql;
 import static ch.avocado.share.common.constants.sql.Tables.*;
 
 public class FileConstants {
-
+    private static final String SELECT_FILES = "" +
+            "SELECT o.id, title, description, last_changed, creation_date, path, module_id, owner.owner_id, extension, mimetype " +
+            "   FROM file AS f " +
+            "JOIN access_control AS o " +
+            "   ON f.id = o.id " +
+            "JOIN " + UPLOADED_TABLE + " AS u " +
+            "   ON u.file_id = f.id " +
+            "JOIN " + OWNERSHIP_TABLE + " AS owner " +
+            "   ON owner.object_id = f.id ";
     public static final String INSERT_QUERY = "INSERT INTO " + FILE_TABLE + " (id, title, last_changed, path, extension, mimetype) VALUES (?, ?, ?, ?, ?, ?)";
     public static final String UPDATE_QUERY = "UPDATE " + FILE_TABLE + " SET title=?, last_changed=?, path=?, extension=?, mimetype=? WHERE id = ?";
-    public static final String SEARCH_QUERY_START = "SELECT o.id, title, description, last_changed, creation_date, path FROM "  + FILE_TABLE + " AS f JOIN access_control AS o ON f.id = o.id WHERE ";
+    public static final String SEARCH_QUERY_START = SELECT_FILES + " WHERE ";
     public static final String SEARCH_QUERY_LIKE= " title LIKE (?) OR description LIKE (?)";
     public static final String SEARCH_QUERY_LINK = " OR";
     public static final String INSERT_UPLOADED_QUERY = "INSERT INTO " + UPLOADED_TABLE + " (file_id, module_id) VALUES (?, ?)";
@@ -18,15 +26,7 @@ public class FileConstants {
     public static final int UPDATE_UPLOADED_INDEX_FILE = 2;
 
 
-    private static final String SELECT_FILES = "" +
-            "SELECT o.id, title, description, last_changed, creation_date, path, module_id, owner.owner_id, extension, mimetype " +
-            "   FROM file AS f " +
-            "JOIN access_control AS o " +
-            "   ON f.id = o.id " +
-            "JOIN " + UPLOADED_TABLE + " AS u " +
-            "   ON u.file_id = f.id " +
-            "LEFT JOIN " + OWNERSHIP_TABLE + " AS owner " +
-            "   ON owner.object_id = f.id ";
+
     public static final String SELECT_BY_TITLE_QUERY_AND_MODULE = SELECT_FILES + "WHERE title = ? AND module_id = ?";
     public static final String SELECT_BY_ID_QUERY = SELECT_FILES + "WHERE o.id = ?";
 }
